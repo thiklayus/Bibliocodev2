d25cf193cad8c0318c5b1b2107a9fd3648006bc4
thiklayus <thiklayus.bibliocode@gmail.com>
2026-02-15 23:12:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BiblePage from "./pages/BiblePage";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
