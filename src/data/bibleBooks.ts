82ee3e636e6de0b18ee39c6347acfd65c9047f53
thiklayus <thiklayus.bibliocode@gmail.com>
2026-02-15 21:31:11 -0300

    Create bibleBooks.ts

---
+++ src/data/bibleBooks.ts
@@ -0,0 +1,13 @@
+export const bibleBooks = [
+  { name: "Gênesis", apiName: "genesis", chapters: 50 },
+  { name: "Êxodo", apiName: "exodus", chapters: 40 },
+  { name: "Salmos", apiName: "psalms", chapters: 150 },
+  { name: "Provérbios", apiName: "proverbs", chapters: 31 },
+  { name: "Isaías", apiName: "isaiah", chapters: 66 },
+  { name: "Mateus", apiName: "matthew", chapters: 28 },
+  { name: "Marcos", apiName: "mark", chapters: 16 },
+  { name: "Lucas", apiName: "luke", chapters: 24 },
+  { name: "João", apiName: "john", chapters: 21 },
+  { name: "Romanos", apiName: "romans", chapters: 16 },
+  { name: "Apocalipse", apiName: "revelation", chapters: 22 }
+];
