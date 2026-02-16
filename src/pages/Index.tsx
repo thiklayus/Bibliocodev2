efd35d480f7a83620615cb5c9968beeaade370aa
thiklayus <thiklayus.bibliocode@gmail.com>
2026-02-16 01:11:19 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,3 @@
-import { useState, useCallback } from 'react';
-import SplashScreen from '@/components/SplashScreen';
-import WelcomeScreen from '@/components/WelcomeScreen';
-
-const Index = () => {
-  const [splashDone, setSplashDone] = useState(false);
-
-  const handleSplashComplete = useCallback(() => {
-    setSplashDone(true);
-  }, []);
-
-  return (
-    <>
-      {!splashDone && <SplashScreen onComplete={handleSplashComplete} />}
-      {splashDone && <WelcomeScreen />}
-    </>
-  );
-};
-
-export default Index;
 import { Navigate } from 'react-router-dom';
 
 const Index = () => {
